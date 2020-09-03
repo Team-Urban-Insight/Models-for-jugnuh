56f41deeeec77b3464cedec37d4c1ead9ae8b72b
Naitik0909 <parmarnaitik0909@gmail.com>
2020-09-03 19:36:32 +0530

    All tables added

--- prac2/orders/models.py
+++ prac2/orders/models.py
@@ -5,6 +5,9 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     amount = models.IntegerField()
 
+    def __str__(self):
+        return self.customer
+
 
 class OrderItem(models.Model):
     duration = models.IntegerField()
@@ -15,3 +18,6 @@ class OrderItem(models.Model):
 
     provider = models.ForeignKey(Service_Provider, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.order.customer
--- prac2/services/models.py
+++ prac2/services/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from basic.models import Service_Provider
+from orders.models import OrderItem
 
 # Create your models here.
 class Service(models.Model):
     service_name = models.CharField(max_length=50)
-    icon = models.ImageField(upload_to="img/icons")
+    icon = models.ImageField(upload_to="img/icons", null=True, blank=True)
     description = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.service_name
 
 class Service_Category(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
@@ -13,3 +17,19 @@ class Service_Category(models.Model):
     cost = models.PositiveIntegerField()
     cost_per_unit = models.CharField(max_length=20)
     category_description = models.CharField(max_length=2000)
+
+    def __str__(self):
+        new_str = str(self.service)+"- "+str(self.category_name)
+        return new_str
+
+class Schedule(models.Model):
+    end_date_time = models.DateTimeField()
+
+    class Meta:
+        unique_together = ['provider', 'order_item']
+
+    provider = models.OneToOneField(Service_Provider, on_delete=models.CASCADE)
+    order_item = models.OneToOneField(OrderItem, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.provider
